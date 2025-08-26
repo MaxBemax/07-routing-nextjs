8df387e90a762763b50931673b74e7655ee074d6
Max Bemax <7510332@gmail.com>
2025-08-26 16:51:28 +0300

    correction in structure

--- app/notes/page.tsx
+++
@@ -1,7 +0,0 @@
-import { fetchNotes } from '@/lib/api';
-import Notes from './filter/[...slug]/Notes.client';
-
-export default async function NotesPage() {
-  const initialData = await fetchNotes(1, 10, '');
-  return <Notes initialData={initialData} />;
-}
