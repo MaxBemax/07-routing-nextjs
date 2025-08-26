79ec6872389cc3a13071ceaf7724ffb6654591ee
Max Bemax <7510332@gmail.com>
2025-08-26 13:14:16 +0300

    changes in NotesPage

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,5 @@
 import { fetchNotes } from '@/lib/api';
-import Notes from './Notes.client';
+import Notes from './filter/[...slug]/Notes.client';
 
 export default async function NotesPage() {
   const initialData = await fetchNotes(1, 10, '');
